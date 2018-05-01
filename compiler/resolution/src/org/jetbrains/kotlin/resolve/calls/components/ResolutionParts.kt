a4a90c6a53db68dc22c56beca41d04fefbf73657
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2018-05-01 23:46:15 +0300

    [NI] Prototype for SAM-conversion.
    Supported:
    - conversion in resolution parts. Also sam-with-receiver is supported automatically
    - by default conversion also enabled for kotlin functions
    
    TODO:
    - turn off synthetic scope with SAM adapter functions
    - add information about arguments with SAM conversion into Candidate (it is argument information because parameter can be vararg)
    - add compatibility logic into OverloadConflictResolver
    - add information about SAM conversion into result ResolvedCall or binding trace
    - support such conversions in BE
    - support case when parameter type is T, T <:> Runnable
    - do not run conversions for kotlin function if LV < 1.3

--- compiler/resolution/src/org/jetbrains/kotlin/resolve/calls/components/ResolutionParts.kt
+++ compiler/resolution/src/org/jetbrains/kotlin/resolve/calls/components/ResolutionParts.kt
@@ -5,6 +5,8 @@
 
 package org.jetbrains.kotlin.resolve.calls.components
 
+import org.jetbrains.kotlin.builtins.functions.FunctionClassDescriptor
+import org.jetbrains.kotlin.builtins.getFunctionalClassKind
 import org.jetbrains.kotlin.descriptors.*
 import org.jetbrains.kotlin.resolve.calls.components.TypeArgumentsToParametersMapper.TypeArgumentsMapping.NoExplicitArguments
 import org.jetbrains.kotlin.resolve.calls.inference.ConstraintSystemOperation
@@ -22,6 +24,7 @@ import org.jetbrains.kotlin.resolve.calls.tower.InvokeConventionCallNoOperatorMo
 import org.jetbrains.kotlin.resolve.calls.tower.VisibilityError
 import org.jetbrains.kotlin.types.ErrorUtils
 import org.jetbrains.kotlin.types.UnwrappedType
+import org.jetbrains.kotlin.types.checker.anySuperTypeConstructor
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 
 internal object CheckInstantiationOfAbstractClass : ResolutionPart() {
@@ -29,7 +32,8 @@ internal object CheckInstantiationOfAbstractClass : ResolutionPart() {
         val candidateDescriptor = resolvedCall.candidateDescriptor
 
         if (candidateDescriptor is ConstructorDescriptor &&
-            !callComponents.statelessCallbacks.isSuperOrDelegatingConstructorCall(resolvedCall.atom)) {
+            !callComponents.statelessCallbacks.isSuperOrDelegatingConstructorCall(resolvedCall.atom)
+        ) {
             if (candidateDescriptor.constructedClass.modality == Modality.ABSTRACT) {
                 addDiagnostic(InstantiationOfAbstractClass)
             }
@@ -231,7 +235,37 @@ private fun KotlinResolutionCandidate.prepareExpectedType(
 ): UnwrappedType {
     val argumentType = argument.getExpectedType(candidateParameter, callComponents.languageVersionSettings)
     val resultType = knownTypeParametersResultingSubstitutor?.substitute(argumentType) ?: argumentType
-    return resolvedCall.substitutor.substituteKeepAnnotations(resultType)
+    val typeInTermsFreshTypes = resolvedCall.substitutor.substituteKeepAnnotations(resultType)
+    return applySAMConversionIfNeeded(typeInTermsFreshTypes, argument)
+}
+
+private fun KotlinResolutionCandidate.applySAMConversionIfNeeded(
+    expectedType: UnwrappedType,
+    argument: KotlinCallArgument
+): UnwrappedType {
+    val argumentIsFunctional = when (argument) {
+        is SimpleKotlinCallArgument -> argument.receiver.let { it.unstableType ?: it.stableType }.isSubtypeOfFunctionType()
+        is LambdaKotlinCallArgument, is CallableReferenceKotlinCallArgument -> true
+        else -> false
+    }
+    if (!argumentIsFunctional) return expectedType
+
+    // todo: support case T <:> Runnable
+    if (csBuilder.isTypeVariable(expectedType)) return expectedType
+
+    val transformedType = callComponents.samTypeTransformer.getFunctionTypeForPossibleSamType(expectedType)
+
+    if (transformedType != null) {
+        // todo write information about SAM conversion for this parameter
+        return transformedType
+    }
+    return expectedType
+}
+
+private fun UnwrappedType.isSubtypeOfFunctionType() = anySuperTypeConstructor {
+    val kind = it.declarationDescriptor?.getFunctionalClassKind()
+    kind == FunctionClassDescriptor.Kind.Function ||
+            kind == FunctionClassDescriptor.Kind.SuspendFunction
 }
 
 internal object CheckReceivers : ResolutionPart() {
