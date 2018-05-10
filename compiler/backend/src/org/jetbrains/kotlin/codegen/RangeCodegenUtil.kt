422abb173b53c7a010c9e322acb82a13a31d17db
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2018-05-10 15:40:55 +0300

    KT-24156 Do not optimize for-loops over Strings with custom iterator
    'fun CharSequence.iterator()' is an extension function, so one can
    overload it with custom implementation.
    Other "predefined" containers such as arrays and ranges have member
    'fun iterator()', so these containers are not affected.
    
    Check that 'iterator' call corresponds to an extension function
    'iterator' defined in package 'kotlin.text' with a receiver of type
    'kotlin.CharSequence'.

--- compiler/backend/src/org/jetbrains/kotlin/codegen/RangeCodegenUtil.kt
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/RangeCodegenUtil.kt
@@ -265,3 +265,9 @@ fun getAsmRangeElementTypeForPrimitiveRangeOrProgression(rangeCallee: CallableDe
 
     throw AssertionError("Unexpected range type: $rangeType")
 }
+
+fun isCharSequenceIterator(descriptor: CallableDescriptor) =
+    descriptor.isTopLevelExtensionOnType("iterator", "kotlin.text") {
+        it.constructor.declarationDescriptor?.isTopLevelInPackage("CharSequence", "kotlin")
+                ?: false
+    }
--- compiler/tests/org/jetbrains/kotlin/codegen/LightAnalysisModeTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/codegen/LightAnalysisModeTestGenerated.java
@@ -15341,6 +15341,11 @@ public class LightAnalysisModeTestGenerated extends AbstractLightAnalysisModeTes
             runTest("compiler/testData/codegen/box/ranges/forInStringVarUpdatedInLoopBody.kt");
         }
 
+        @TestMetadata("forInStringWithCustomIterator.kt")
+        public void testForInStringWithCustomIterator() throws Exception {
+            runTest("compiler/testData/codegen/box/ranges/forInStringWithCustomIterator.kt");
+        }
+
         @TestMetadata("forIntRange.kt")
         public void testForIntRange() throws Exception {
             runTest("compiler/testData/codegen/box/ranges/forIntRange.kt");
