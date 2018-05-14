55f214ab8554e881020df87a2eca536b0467116d
Denis Zharkov <denis.zharkov@jetbrains.com>
2018-05-14 14:11:17 +0300

    Support androidx under-migration-like nullability annotations
    They are hardcoded to avoid having dependency from android.jar on
    our annotations' jar with UnderMigration.
    
    Even while it could be a compile-only dependency we need to make sure
    that annotated types are read properly without RecentlyNonNull/RecentlyNullable
    in the classpath
    
     #KT-24278 Fixed

---
+++ compiler/testData/foreignAnnotations/tests/androidRecently.kt
@@ -0,0 +1,43 @@
+// !DIAGNOSTICS: -UNUSED_VARIABLE -UNUSED_PARAMETER
+// FILE: A.java
+
+import androidx.annotation.*;
+
+public class A<T> {
+    @RecentlyNullable public String field = null;
+
+    @RecentlyNullable
+    public String foo(@RecentlyNonNull String x, @RecentlyNullable CharSequence y) {
+        return "";
+    }
+
+    @RecentlyNonNull
+    public String bar() {
+        return "";
+    }
+
+    @RecentlyNullable
+    public T baz(@RecentlyNonNull T x) { return x; }
+}
+
+// FILE: main.kt
+
+fun main(a: A<String>, a1: A<String?>) {
+    a.foo("", null)?.length
+    <!NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS!>a.foo("", null)<!>.length
+    <!NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS!>a.foo(<!NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS!>null<!>, "")<!>.length
+
+    a.bar().length
+    a.bar()<!UNNECESSARY_NOT_NULL_ASSERTION!>!!<!>.length
+
+    a.field?.length
+    <!NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS!>a.field<!>.length
+
+    <!NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS!>a.baz("")<!>.length
+    a.baz("")?.length
+    <!NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS!>a.baz(<!NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS!>null<!>)<!>.length
+
+    a1.baz("")!!.length
+    a1.baz(<!NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS!>null<!>)!!.length
+}
+
